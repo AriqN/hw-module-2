e1f5e33ecd26a3b7ea2e6c2de037eef286964f5d
ariqN <ariqnurmansyah9@upi.edu>
2022-03-23 12:08:19 +0700

    add new headline

--- src/App.js
+++ src/App.js
@@ -4,26 +4,31 @@ import data from './data';
 function App() {
   return (
     <div className='container'>
-        <div className='cards'>
-            <div className='card'>
-            <img src= {data.album.images[0].url} alt="Image"/>
-            <div className="text">
-                <div className="song-title">
-                    {data.name}
-                </div>
-                <div className="artist">
-                    {data.artists[0].name}
-                </div>
-                <div className="album">
-                    {data.album.name}
-                </div>
-            </div>    
-            <div className="add">
-                <input type="submit" value="Select"/>                
-            </div>
+      <div className="music">
+                <h1>
+                    Music Made For You
+                </h1>
+          <div className='cards'>
+              <div className='card'>
+              <img src= {data.album.images[0].url} alt="Image"/>
+              <div className="text">
+                  <div className="song-title">
+                      {data.name}
+                  </div>
+                  <div className="artist">
+                      {data.artists[0].name}
+                  </div>
+                  <div className="album">
+                      {data.album.name}
+                  </div>
+              </div>    
+              <div className="add">
+                  <input type="submit" value="Select"/>                
+              </div>
+          </div>
         </div>
-      </div>
       </div>  
+    </div>  
   );
 }
 
